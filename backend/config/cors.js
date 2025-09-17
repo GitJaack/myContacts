cf63c5b2d376bbe6fc96ff62354ba576a572646a
GitJaack <yejack326@gmail.com>
2025-09-17 11:05:17 +0200

    CORS acces url render

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,8 +1,12 @@
 const cors = require("cors");
 
+const allowedOrigin = [
+  "http://localhost:3000",
+  "https://mycontacts-9gnn.onrender.com",
+];
 const corsOptions = {
-  origin: true,
-  methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+  origin: allowedOrigin,
+  methods: ["GET", "POST", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
